c8d18925989d95964484f670802b4fa08ce52ec8
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-07 21:19:07 +0100

    Update number_quessing.py

--- number_quessing.py
+++ number_quessing.py
@@ -1,11 +1,21 @@
 import random
 
-start = -5
-stop = 11
+end_range = input("Podaj liczbę: ") #odpowiedź użytkownika jest stringiem
+
+if end_range.isdigit(): #sprawdzenie czy podany string jest liczbą
+  end_range = int(end_range) #przekonwertowanie stringa do inta 
+
+    if end_range <= 0:
+      print('Następnym razem podaj liczbę większą od zera :)')
+      quit() 
+else:
+  print('Następnym razem podaj prawdziwą liczbę!')
+  quit()
 
 #przedział otwarty z prawej strony
 #jeśli wstawimy tylko jedną liczbę to wygenerowana zostanie liczba z przedziału od 0 do podanej liczby minus 1
-r = random.randrange(start, stop)  
+#randint ma przedział zamknięty z prawej strony
+random_number = random.randint(end_range)  
 print(r)
 
 
