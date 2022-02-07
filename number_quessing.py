84329bc01511bda1596259cedd3c589128b6bc0d
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-07 21:00:23 +0100

    Create number_quessing.py

---
+++ number_quessing.py
@@ -0,0 +1,11 @@
+import random
+
+start = -5
+stop = 11
+
+#przedział otwarty z prawej strony
+#jeśli wstawimy tylko jedną liczbę to wygenerowana zostanie liczba z przedziału od 0 do podanej liczby minus 1
+r = random.randrange(start, stop)  
+print(r)
+
+
