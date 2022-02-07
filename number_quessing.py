758cd61d3677a03a5fa13f22862adfa6b9677e87
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-07 21:29:42 +0100

    Update number_quessing.py

--- number_quessing.py
+++ number_quessing.py
@@ -4,7 +4,6 @@ end_range = input('Podaj ile liczb chcesz wygenerować: ') #odpowiedź użytkown
 
 if end_range.isdigit(): #sprawdzenie czy podany string jest liczbą
   end_range = int(end_range) #przekonwertowanie stringa do inta 
-
     if end_range <= 0:
       print('Następnym razem podaj liczbę większą od zera :)')
       quit() 
@@ -19,7 +18,13 @@ random_number = random.randint(0, end_range)
 
 while True:
   user_quess = input('Spróbuj odgadnąć NASZĄ liczbę: ')
-  break
-  
-  
+  if user_quess.isdigit():
+    end_range = int(user_quess) 
+  else:
+    print('Następnym razem podaj prawdziwą liczbę!')
+    continue #powrót do początku pętli
 
+  if user_quess == random_number:
+    print('Zgadłeś, BRAWO!')
+  else:
+    print('Nie udało się, powodzenia następnym razem :(')
