284a7475dc2bea49598698a7557b730f84c881e1
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-07 21:24:24 +0100

    Update number_quessing.py

--- number_quessing.py
+++ number_quessing.py
@@ -1,6 +1,6 @@
 import random
 
-end_range = input("Podaj liczbę: ") #odpowiedź użytkownika jest stringiem
+end_range = input('Podaj ile liczb chcesz wygenerować: ') #odpowiedź użytkownika jest stringiem
 
 if end_range.isdigit(): #sprawdzenie czy podany string jest liczbą
   end_range = int(end_range) #przekonwertowanie stringa do inta 
@@ -16,6 +16,10 @@ else:
 #jeśli wstawimy tylko jedną liczbę to wygenerowana zostanie liczba z przedziału od 0 do podanej liczby minus 1
 #randint ma przedział zamknięty z prawej strony
 random_number = random.randint(0, end_range)  
-print(random_number)
 
+while True:
+  user_quess = input('Spróbuj odgadnąć NASZĄ liczbę: ')
+  break
+  
+  
 
