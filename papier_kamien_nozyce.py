865a353a5dc4e1f3a537b93b2e01e15ddcbc73c4
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-10 18:30:49 +0100

    Update papier_kamien_nozyce.py

--- papier_kamien_nozyce.py
+++ papier_kamien_nozyce.py
@@ -6,16 +6,19 @@ import random
 user_win = 0 
 computer_underscore = 0
 
+options = ["papier", "kamień", "nożyce"]
+
 while True:
   user_input = print("Wybierz Papier/Kamień/Nożyce albo wciśnij K żeby zakończyć grę: ").lower()
   if user_input == "k":
     break
     
-  if user_input is not in ["papier", "kamień", "nożyce"]:
+  if user_input is not in options:
     continue
     
   random_number = random.randint(0, 2)
-  #
+  # papier: 0, kamień: 1, nożyce: 2
+  computer_pick = options[random_number]
   
   
 print("Do następnego razu! :)")
