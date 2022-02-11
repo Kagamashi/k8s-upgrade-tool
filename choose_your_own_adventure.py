0a301cface5279a36a12387c76479f36f3c9a974
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-11 16:42:27 +0100

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,4 +1,7 @@
 # Became a WITCHER
 
 name = input("Podaj swoje imię podróżniku: ")
-print("Witaj", name, "w swojej nowej przygodzie!")
+print("Witaj", name, "na Wiedźmińskiej scieżce!")
+
+answer = input(
+  "
