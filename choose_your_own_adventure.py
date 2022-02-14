d46851752bcdd92b3e79dfeebfc0b43cd167d3f1
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-14 14:38:13 +0100

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -6,14 +6,14 @@ print("Witaj", name, "w Mechanicznej Pomarańczy")
 answer = input("Mijając próg wejścia przechodzisz obok szatni której pilnuje dobrze zbudowany, pozbawiony włosów na głowie mężczyzna o niezbyt inteligentnym wyrazie twarzy. Zostawiasz kurtkę (TAK) czy postanawiasz prześlizgnąć się w niej unikając wzroku ochroniarza (NIE)?")
   
 if answer == "TAK":
-  answer = input("Podchodzisz do szatni 
+  answer = input("Podchodzisz do szatni i widzisz przepiękną dziewczynę z IBM-u, która prosi Cię o podanie jej kurtki. Masz dwie opcje: podajesz kurtkę, kładziesz 2 złote i kierujesz się w stronę baru (lame) lub prosisz dziewczynę o numer telefonu (chad)?")
                  
-  if answer ==
-    print()
-  elif answer ==
-    print()
+  if answer == "lame"
+    print("Upijasz się z kumplami i zgonujesz na kanapie obok Możejko. PRZEGRYWASZ!")
+  elif answer == "chad"
+    print(")
   else:
-    print("Nie miałeś takiej opcji. Wyrzucają Cię ze studiów :(")
+    print("Nie miałeś takiej opcji. Wyrzucają Cię z Pomarańczy :(")
                  
                  
 elif answer == "NIE":
@@ -24,7 +24,7 @@ elif answer == "NIE":
   elif answer == ""
     print()
   else:
-    print("Nie miałeś takiej opcji. Wyrzucają Cię ze studiów :(")
+    print("Nie miałeś takiej opcji. Wyrzucają Cię z Pomarańczy :(")
                  
 else:
-  print("Nie miałeś takiej opcji. Wyrzucają Cię ze studiów :(")
+  print("Nie miałeś takiej opcji. Wyrzucają Cię z Pomarańczy :(")
