fe6fbcf6dc68b8038e50fab99ea1e9129aadf129
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-14 19:06:25 +0100

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -4,5 +4,10 @@
 master_pwd = input("What is the master password? ")
 
 action = input("Would you add new password or view the existing ones? (add/view)")
+if action == "add":
 
+elif action =="view":
+  
+else: 
+  "Wrong option, try again."
 
