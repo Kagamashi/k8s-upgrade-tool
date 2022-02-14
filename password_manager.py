a29c9a402c3e9cea1fa85518097b8fa7a3755c89
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-14 14:53:34 +0100

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,8 @@
-# Zarządzanie hasłami 
+# Baza przechowywująca login i hasło do różnych witryn internetowych. Hasła są zaszyfrowane aby zabezpieczyć je przed niepowołanym dostępem.
+# TO TYLKO PROJEKT DO NAUKI, RADZĘ TEGO NIE UŻYWAĆ DO PRZECHOWYWANIA SWOICH PRAWDZIWYCH HASEŁ
+
+master_pwd = input("What is the master password? ")
+
+
+
+
