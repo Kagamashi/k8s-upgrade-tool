f2da48e599702357acb7fda06f5bb055a2981c23
Mikołaj <mikolaj.borecki1@gmail.com>
2022-02-15 23:00:37 +0100

    Update password_manager(with encryption).py

--- password_manager(with encryption).py
+++ password_manager(with encryption).py
@@ -32,7 +32,7 @@ def view():
     for line in f.readlines():
       data = line.rstrip() 
       user, passw = data.split("|")
-      print("User:", user, "| Password:", fer.decrypt(passw.encode()))
+      print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
       
       
 while True:
