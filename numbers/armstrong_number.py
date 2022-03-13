37e39408ef0cd14732715f47cf4424cf489b608e
Mikołaj <mikolaj.borecki1@gmail.com>
2022-03-13 19:11:25 +0100

    Update armstrong_number.py

--- numbers/armstrong_number.py
+++ numbers/armstrong_number.py
@@ -1,24 +1,34 @@
 # Armstrong number - it is a number which is equal to the sum of cube of its digits
 # Example: 153 -> 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 
 
-i = 0 
-result = 0
+num = int(input("Enter a number: ")) #take input from the user
 
-n = int(input("Proszę podaj liczbę do sprawdzenia: "))
+sum = 0 #initialize sum
 
-number1 = n
-temp = n
+temp = num 
 
-while n!=0
-  n = (n/10)
-  i=i+1;
-
-while number1!=0:
-  n=number1%10
-  result=result+pow(n,i)
-  number1=number1//10
+while temp > 0:
+  digit = temp % 10
+  sum += digit ** 3
+  temp //= 10
   
-if temp==result:
-  print("Number is an Armstrong!")
-else: 
-  print("Number is NOT an Armstrong.")
+if num == sum:
+  print(num, "is an Armstrong number")
+else:
+  print(num, "is not an Armstrong number")
+
+''' Wyjaśnienie działania algorytmu:
+Podajemy 153 -> Temp = 153 
+digit = 153 % 10 (czyli mamy resztę z modulo) = 3
+powiększamy sumę o 3 podniesione do potęgi 3 -> czyli sum = 27
+Dzielimy 153 na 10 i pomijamy to co jest po przecinku otrzymując 15
+
+Temp = 15
+Digit = 15 % 10 czyli 5
+Zwiększamy sumę o 5^3 czyli 27 + 125 = 152
+Dzielimy 15 na 10 i otrzymujemy 1 
+
+Temp = 1
+Digit = 1 % 10 czyli 1
+Dodajemy do sumy 1^3 czyli sum = 152 + 1 = 153
+''' 
